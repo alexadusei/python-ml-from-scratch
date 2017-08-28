1297937dda6651e3d525ccaf14a493d942aa6b61
Alex Adusei <alex.adusei@gmail.com>
2017-08-27 20:16:19 -0700

    Complete asn2 (regularized)

--- asn2-logistic-regression/asn2/ex2.py
+++ asn2-logistic-regression/asn2/ex2.py
@@ -6,12 +6,18 @@ import scipy.optimize as opt
 
 def main():
     run_logistic_regression('/data/ex2data1.txt')
+    run_logistic_regression('/data/ex2data2.txt', 'reg')
 
 
-def run_logistic_regression(path):
-    X, y, theta = preprocess_data(path)
+def run_logistic_regression(path, type='normal'):
+    if type == 'normal':
+        X, y, theta = preprocess_data(path)
+        data_params = (X, y)
+    else:
+        X, y, theta, reg_lambda = preprocess_data_reg(path)
+        data_params = (X, y, reg_lambda)
 
-    predictions = run_predictions(X, y, theta)
+    predictions = run_predictions(data_params, theta)
     correct = ([1 if ((a == 1 and b == 1) or (a == 0 and b == 0)) else 0
                 for (a, b) in zip(predictions, y)])
 
@@ -20,7 +26,9 @@ def run_logistic_regression(path):
     # will always be the numerator. No need for float conversions or scaling.
     accuracy = sum(correct) % len(correct)
 
+    print "\n-----------------------------------------------------"
     print 'Unregularized Logistic Regression Accuracy = {0}%'.format(accuracy)
+    print "-----------------------------------------------------\n"
 
 
 def preprocess_data(path):
@@ -41,23 +49,69 @@ def preprocess_data(path):
 
     return X, y, theta
 
-def get_data(path_arg):
+
+def preprocess_data_reg(path):
+    data = get_data(path, 'reg')
+
+    # because this is a more complicated dataset, there is no linear boundary
+    # here to draw a straight line. One way to deal with this is using a
+    # complex polynomial with logistic regression to construct features in
+    # the shape of a circle. We'll arbitrarily create many higher-dimensional
+    # polynomials
+
+    degree = 5
+    x1 = data['Test 1']
+    x2 = data['Test 2']
+
+    data.insert(3, 'Ones', 1)
+
+    for i in range(1, degree):
+        for j in range(0, i):
+            data['F' + str(i) + str(j)] = np.power(x1, i - j) * np.power(x2, j)
+
+    # remove the first two test columns. This makes 'Accepted' the first column
+    # and 'Ones' the second column
+    data.drop('Test 1', axis=1, inplace=True)
+    data.drop('Test 2', axis=1, inplace=True)
+
+    num_cols = data.shape[1]
+    X = data.iloc[:, 1:num_cols]
+    y = data.iloc[:, :1]
+
+    # convert numpy arrays and initialize parameter array theta
+    X = np.array(X.values)
+    y = np.array(y.values)
+    theta = np.zeros(num_cols - 1)
+
+    # initialize value for regularization parameber lambda
+    reg_lambda = 1
+
+    return X, y, theta, reg_lambda
+
+def get_data(path_arg, type='normal'):
     pwd = os.getcwd()
     path = pwd + path_arg
 
-    return pd.read_csv(path, header=None, names=[
-                       'Exam 1', 'Exam 2', 'Admitted'])
+    if type == 'normal':
+        return pd.read_csv(path, header=None, names=[
+                           'Exam 1', 'Exam 2', 'Admitted'])
+    else:
+        return pd.read_csv(path, header=None, names=[
+                           'Test 1', 'Test 2', 'Accepted'])
 
 
-def run_predictions(X, y, theta):
+# args here includes X, y, and reg_lambda depending on type of log. regression
+def run_predictions(args, theta):
+    X = args[0]
+
     theta = np.matrix(theta)
-    optimized_theta = np.matrix(find_optimal_theta(X, y, theta))
+    optimized_theta = np.matrix(find_optimal_theta(args, theta))
 
     probability = compute_hypothesis(X * optimized_theta.T)
     return [1 if x >= 0.5 else 0 for x in probability]
 
 
-def find_optimal_theta(X, y, theta):
+def find_optimal_theta(args, theta):
     # Logistic Regression doesn't use the traditional gradient descent
     # approach of stepping through multiple attempts to lower our cost
     # function. Instead, we'll use a built-in, optimized tool from scipy to
@@ -70,12 +124,16 @@ def find_optimal_theta(X, y, theta):
     # NOTE: The ordering or parameters for your cost and gradient functions are
     # important here. Should be theta, X and y in that order (see arg list for
     # those two functions)
+    cost_func = compute_cost if len(args) == 2 else compute_cost_reg
+    gradient_func = (compute_gradient_descent if len(args) == 2 else
+                    compute_gradient_descent_reg)
+
     result = opt.fmin_tnc(
-                        func=compute_cost,
-                        x0=theta,
-                        fprime=compute_gradient_descent,
-                        args=(X, y)
-                        )
+                func=cost_func,
+                x0=theta,
+                fprime=gradient_func,
+                args=(args)
+                )
 
     return result[0]
 
@@ -95,6 +153,19 @@ def compute_cost(theta, X, y):
 
     return (1.0 / m) * np.sum(np.multiply(-y, np.log(h)) - np.multiply((1 - y), np.log(1 - h)))
 
+def compute_cost_reg(theta, X, y, reg_lambda):
+    theta = np.matrix(theta)
+    X = np.matrix(X)
+    y = np.matrix(y)
+    h = compute_hypothesis(X * theta.T)
+    m = len(X)
+
+    # don't regularize the first theta term
+    reg = (reg_lambda / (2 * m)) * np.sum(np.power(theta[:, 1:], 2))
+
+    return (((1.0 / m) * np.sum(np.multiply(-y, np.log(h))
+                - np.multiply((1 - y), np.log(1 - h)))) + reg)
+
 
 def compute_gradient_descent(theta, X, y):
     theta = np.matrix(theta)
@@ -109,6 +180,26 @@ def compute_gradient_descent(theta, X, y):
     # (3, 100) x (100, 1)
     return np.array((1.0 / m) * (X.T * (h - y)))
 
+
+def compute_gradient_descent_reg(theta, X, y, reg_lambda):
+    theta = np.matrix(theta)
+    X = np.matrix(X)
+    y = np.matrix(y)
+    m = len(X)
+
+    num_parameters = theta.flatten().shape[1]
+    grad = np.zeros(num_parameters)
+
+    for i in range(num_parameters):
+        h = compute_hypothesis(X * theta.T)
+        term = np.multiply(h - y, X[:, i])
+
+        if i == 0:
+            grad[i] = np.sum(term) / m
+        else:
+            grad[i] = (np.sum(term) / m) + ((reg_lambda / m) * theta[:, i])
+
+    return grad
 
 if __name__ == '__main__':
     main()
